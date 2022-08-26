c47f9f55d22ccfbcd5119a8864a437fdebf31207
marvin espira <espiramarvin@gmail.com>
2022-08-26 12:01:35 +0300

    add item post request

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 
 type todo struct {
 	ID        string `json:"id"`
-	Item      string `json:"title"`
+	Item      string `json:"item"`
 	Completed bool   `json:"completed"`
 }
 
@@ -18,12 +18,28 @@ var todos = []todo{
 	{ID: "3", Item: "Record Video", Completed: false},
 }
 
+// get all todos
 func getTodos(context *gin.Context) {
 	context.IndentedJSON(http.StatusOK, todos)
+}
+
+// add todo
+func addTodo(context *gin.Context) {
+	var newTodo todo
+
+	if err := context.BindJSON(&newTodo); err != nil {
+		return
+	}
+
+	todos = append(todos, newTodo)
+
+	context.IndentedJSON(http.StatusCreated, newTodo)
+
 }
 
 func main() {
 	router := gin.Default() // creates server
 	router.GET("/todos", getTodos)
+	router.POST("/todos", addTodo)
 	router.Run("localhost:4000")
 }
