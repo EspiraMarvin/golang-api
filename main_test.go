26aa0c7aa6fe95937f3d8c08356ecde40aec2b4d
marvin espira <espiramarvin@gmail.com>
2022-08-26 21:10:17 +0300

    adds tests

---
+++ main_test.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
+)
+
+// func setupRouter() *gin.Engine {
+// 	router := gin.Default()
+// 	return router
+// }
+
+func setupRouter() *gin.Engine {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.String(200, "pong")
+	})
+	return r
+}
+
+func TestHomepageHandler(t *testing.T) {
+	mockResponse := `{" message": "Welcome to the Todo APIs with Golang" }`
+	r := setupRouter()
+	r.GET("/", HomepageHandler)
+	req, _ := http.NewRequest("GET", "/", nil)
+	w := httptest.NewRecorder()
+	r.ServeHTTP(w, req)
+
+	responseData, _ := ioutil.ReadAll(w.Body)
+	assert.Equal(t, mockResponse, string(responseData))
+	assert.Equal(t, http.StatusOK, w.Code)
+}
+
+// func main() {
+// 	r := setupRouter()
+// 	r.Run(":8080")
+// }
