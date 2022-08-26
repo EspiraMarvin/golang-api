e77e3cbf028d6949e7a7b37156dce4133d6a7ba8
marvin espira <espiramarvin@gmail.com>
2022-08-26 11:29:53 +0300

    creates server and getAll endpoint

--- main.go
+++ main.go
@@ -1,9 +1,10 @@
 package main
 
-// import (
-// 	"net/http"
+import (
+	"net/http"
 
-// )
+	"github.com/gin-gonic/gin"
+)
 
 type todo struct {
 	ID        string `json:"id"`
@@ -15,4 +16,14 @@ var todos = []todo{
 	{ID: "1", Item: "Clean Room", Completed: false},
 	{ID: "2", Item: "Read Book", Completed: false},
 	{ID: "3", Item: "Record Video", Completed: false},
+}
+
+func getTodos(context *gin.Context) {
+	context.IndentedJSON(http.StatusOK, todos)
+}
+
+func main() {
+	router := gin.Default() // creates server
+	router.GET("/todos", getTodos)
+	router.Run("localhost:4000")
 }
