bb58a8214c31d21e0bdab820c85be8c9fe42009b
marvin espira <espiramarvin@gmail.com>
2022-08-26 12:21:46 +0300

    adds toggle todo item

--- main.go
+++ main.go
@@ -43,6 +43,7 @@ func getTodo(context *gin.Context) {
 	id := context.Param("id")
 	todo, err := getTodoById(id)
 
+	// if todo doesn't exist we throw an error
 	if err != nil {
 		context.IndentedJSON(http.StatusNotFound, gin.H{"message": "Todo Not Found"})
 	}
@@ -51,7 +52,7 @@ func getTodo(context *gin.Context) {
 
 }
 
-// get specific todo
+// get todo by its id
 func getTodoById(id string) (*todo, error) {
 	for i, t := range todos {
 		if t.ID == id {
@@ -61,10 +62,27 @@ func getTodoById(id string) (*todo, error) {
 	return nil, errors.New("todo not found")
 }
 
+// update todo
+
+func toggleTodoStatus(context *gin.Context) {
+	id := context.Param("id")
+	todo, err := getTodoById(id)
+
+	// if todo doesn't exist we throw an error
+	if err != nil {
+		context.IndentedJSON(http.StatusNotFound, gin.H{"message": "Todo Not Found"})
+	}
+
+	todo.Completed = !todo.Completed
+
+	context.IndentedJSON(http.StatusOK, todo)
+}
+
 func main() {
 	router := gin.Default() // creates server
 	router.GET("/todos", getTodos)
 	router.GET("/todos/:id", getTodo)
+	router.PATCH("/todos/:id", toggleTodoStatus)
 	router.POST("/todos", addTodo)
 	router.Run("localhost:4000")
 }
